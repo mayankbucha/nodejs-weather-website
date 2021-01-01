684da8ad8b402993ab995707e55d95f4e55e2a37
Mayank <mayankbucha01@gmail.com>
2021-01-01 19:04:44 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public') 
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and view location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Mayank',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Mayank'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This site is used to provide weather info',
+        title: 'Help',
+        name: 'Mayank'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: 'You must provide an address',
+        })
+    }
+    const address = req.query.address
+    geocode(address, (error, data) => {
+        if (error) {
+            return res.send({
+                error,
+            })
+        }
+
+        forecast(data, (error, forecastData) => {
+            if (error) {
+                return res.send({
+                    error,
+                })
+            }
+            return res.send({
+                forecast: forecastData,
+                location: data.location,
+                address: req.query.address,
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term',
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: [],
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Mayank',
+        errorMessage: 'Help article not found' 
+    })
+})
+
+app.get('*', (req, res) => { 
+    res.render('404', {
+        title: '404',
+        name: 'Mayank',
+        errorMessage: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
